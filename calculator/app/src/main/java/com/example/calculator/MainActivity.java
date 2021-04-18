9b0c6e9bb15c1cdbe5dcb29abd074caa1476aa4d
Jordan <k7nsuy2@gmail.com>
2021-04-18 21:57:23 +0900

    22
    22

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button[] btns = new Button[16];
     int[] btn = {R.id.btn0,R.id.btn1,R.id.btn2,R.id.btn3,
@@ -23,10 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("계산기");
 
-        tView = findViewById(R.id.tView01);
+        tView = findViewById(R.id.tView);
+
         for(int i = 0; i < btns.length; i++) {
             btns[i] = findViewById(btn[i]);
+            btns[i].setOnClickListener(this);
             }
     }
 
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.btn0) {
+            tView.setText("0");
+        } else if (v.getId() == R.id.btn1) {
+            tView.setText("1");
+        }
+    }
 }
